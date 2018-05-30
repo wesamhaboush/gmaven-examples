de0c5b1e7d398c471a9129eb6da5698df1bef036
shaboush <shaboush@aconex.com>
2018-05-30 13:38:17 -0700

    running groovy tests from java project

---
+++ gmaven-samples/src/test/groovy/net/codebreeze/gmaven/GAppTest.groovy
@@ -0,0 +1,12 @@
+package net.codebreeze.gmaven
+
+import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.assertTrue
+
+class GAppTest {
+    @Test
+    void testApp() {
+        assertTrue true
+    }
+}
